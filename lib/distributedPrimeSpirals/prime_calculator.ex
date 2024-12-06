921ed40221a8544e7108adaf3cfab5e5e1bda5ce
Wi42 <101128608+Wii42@users.noreply.github.com>
2024-12-06 15:22:03 +0100

    added PrimeCalculator

--- lib/distributedPrimeSpirals/prime_calculator.ex
+++ lib/distributedPrimeSpirals/prime_calculator.ex
@@ -1,5 +1,15 @@
 defmodule PrimeCalculator do
-  def find_primes(n) do
+  @doc """
+  Finds all primes from 0 to .
+
+  notifier is called when an event happens. It takes two arguments, event and message. Message might be nil.
+  Possible events are:
+  - :primes_done - all primes up to n were found
+  - :new_prime - a new prime number was found, message is the found prime. Example: (:new_prime, 7)
+  - :checked_ranges - how the range 0..n is divided up to allow concurrent computation
+  """
+  @spec find_primes(number(), (atom(), any() -> :ok)) :: :ok
+  def find_primes(n, notifier) do
     number_of_nodes = 16
     range_size = ((n + 1) / number_of_nodes) |> ceil()
 
@@ -7,30 +17,26 @@ defmodule PrimeCalculator do
       for node <- 0..(number_of_nodes - 1),
           do: generate_range(node, range_size, n)
 
-    IO.inspect(range_list)
-
-    #processes = for range <- range_list, do: fn -> check_chunk_for_primes(range) end
+    notifier.(
+      :checked_ranges,
+      Enum.map(range_list, fn first..last//_ -> "#{first}..#{last}" end) |> Enum.join(", ")
+    )
 
     Task.async_stream(range_list, fn range ->
-      check_chunk_for_primes(range)
-    end) |> Enum.to_list()
+      check_chunk_for_primes(range, notifier)
+    end)
+    |> Enum.to_list()
 
-    #PubSub.broadcast(PrimeSpirals.PubSub, @topic, :primes_done)
-    IO.puts("All primes up to #{n} found")
-
-    #Task.start(fn ->
-    #  for p <- processes, do: p.()
-    #  # for i <- 0..n, do: check_number(i)
-    #  PubSub.broadcast(PrimeSpirals.PubSub, @topic, :primes_done)
-    #  IO.puts("All primes up to #{n} found")
-    #end)
+    notifier.(:primes_done, "All primes up to #{n} found")
+    # PubSub.broadcast(PrimeSpirals.PubSub, @topic, :primes_done)
+    # IO.puts("All primes up to #{n} found")
   end
 
   @doc """
   Generates a range, ensuring that all generated ranges are disjunct and numbers are <= max_value
 
   """
-  defp generate_range(node, range_size, max_value) do
+  def generate_range(node, range_size, max_value) do
     first = node * range_size
     last = (node + 1) * range_size - 1
 
@@ -39,17 +45,18 @@ defmodule PrimeCalculator do
     if first <= max_value, do: Range.new(first, min(last, max_value)), else: 1..0//1
   end
 
-  defp check_chunk_for_primes(range) do
-    for i <- range, do: check_number(i)
+  defp check_chunk_for_primes(range, notifier) do
+    for i <- range, do: check_number(i, notifier)
     range
   end
 
-  defp check_number(i) do
+  defp check_number(i, notifier) do
     p = is_prime?(i)
 
     if p == true do
-      IO.inspect("new prime: #{i}")
-      #PubSub.broadcast(PrimeSpirals.PubSub, @topic, {:new_count, i})
+      notifier.(:new_prime, i)
+      # IO.inspect("new prime: #{i}")
+      # PubSub.broadcast(PrimeSpirals.PubSub, @topic, {:new_count, i})
     end
   end
 
@@ -66,4 +73,4 @@ defmodule PrimeCalculator do
   end
 end
 
-PrimeCalculator.find_primes(1000)
+PrimeCalculator.find_primes(1000, fn event, msg -> IO.puts("#{event}: #{msg}") end)
