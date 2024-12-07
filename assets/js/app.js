5e325f0a40dc1e841b46244965c7a14bbf8ced1c
oliolioli <oli.meier@bluewin.ch>
2024-12-07 18:35:35 +0100

    Clean code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,3 @@
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
@@ -171,5 +154,3 @@ export function addPointToGrid(x, y) {
 
   addPoint(x, y, 0);
 }
-
-//document.addEventListener("DOMContentLoaded", initThreeJs);
