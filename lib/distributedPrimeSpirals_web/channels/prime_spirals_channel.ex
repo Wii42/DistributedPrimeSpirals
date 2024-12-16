fd8d49f1cd933402dd07fe282f6e8df52ffcf197
oliolioli <oli.meier@bluewin.ch>
2024-12-16 23:21:53 +0100

    Clean code

--- lib/distributedPrimeSpirals_web/channels/prime_spirals_channel.ex
+++ lib/distributedPrimeSpirals_web/channels/prime_spirals_channel.ex
@@ -15,6 +15,11 @@ defmodule DistributedPrimeSpiralsWeb.PrimeSpiralsChannel do
   end
 
   @impl true
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast!(socket, "new_msg", %{body: body})
+    {:noreply, socket}
+  end
+
   def handle_in("new_prime", %{"body" => body}, socket) do
     broadcast!(socket, "new_prime", %{body: body})
     {:noreply, socket}
